243812f917a8801587d08723dfe61c6f2d31d535
anjandey-tech <63156047+anjandey-tech@users.noreply.github.com>
2021-06-14 21:10:07 +0530

    forth commit(video and image text is not showing but rest are fine,everything is download for whatapp )

--- app/src/main/java/com/inturnsala/mystorysaver/MainActivity.java
+++ app/src/main/java/com/inturnsala/mystorysaver/MainActivity.java
@@ -3,10 +3,18 @@ package com.inturnsala.mystorysaver;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.inturnsala.mystorysaver.databinding.ActivityMainBinding;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class  MainActivity extends AppCompatActivity {
 private ActivityMainBinding binding;
@@ -19,5 +27,24 @@ private ActivityMainBinding binding;
           startActivity(new Intent(this,WhatsappActivity.class));
 
         });
+        checkpermission();
+    }
+
+    private void checkpermission()
+    {
+        Dexter.withContext(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if(!report.areAllPermissionsGranted())
+                    checkpermission();
+
+
+            }
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
+        }).check();
     }
 }
