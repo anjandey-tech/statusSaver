dcac95fc5366cd41538b9355a225c32a16ce337c
anjandey-tech <63156047+anjandey-tech@users.noreply.github.com>
2021-06-14 10:13:53 +0530

    second commit(prb 4th video error: package org.jetbrains.annotations does not exist so i delete this line)

--- app/src/main/java/com/inturnsala/mystorysaver/WhatsappActivity.java
+++ app/src/main/java/com/inturnsala/mystorysaver/WhatsappActivity.java
@@ -1,6 +1,11 @@
 package com.inturnsala.mystorysaver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
 
@@ -11,4 +16,42 @@ public class WhatsappActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whatsapp);
     }
+
+//     class viewpagerAdapter extends FragmentStateAdapter{
+//
+//         public viewpagerAdapter(@NonNull @org.jetbrains.annotations.NotNull FragmentManager fragmentManager, @NonNull @org.jetbrains.annotations.NotNull Lifecycle lifecycle) {
+//             super(fragmentManager, lifecycle);
+//         }
+//
+//         @NonNull
+//         @org.jetbrains.annotations.NotNull
+//         @Override
+//         public Fragment createFragment(int position) {
+//             return null;
+//         }
+//
+//         @Override
+//         public int getItemCount() {
+//             return 0;
+//         }
+//     }
+//
+//    class viewpagerAdapter extends FragmentStateAdapter{
+//
+//        public viewpagerAdapter(@NonNull   FragmentManager fragmentManager, @NonNull   Lifecycle lifecycle) {
+//            super(fragmentManager, lifecycle);
+//        }
+//
+//
+//
+//        @Override
+//        public Fragment createFragment(int position) {
+//            return null;
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return 0;
+//        }
+//    }
 }
